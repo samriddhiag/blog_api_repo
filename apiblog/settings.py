ec3c613819a342b06138d90d9a93118bbc1797fe
Akshat Gupta <akshatvg23@gmail.com>
2020-06-20 16:19:42 +0530

    django_heroku

--- apiblog/settings.py
+++ apiblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT= os.path.join(PROJECT_DIR, 'static')
+
+django_heroku.settings(locals())
